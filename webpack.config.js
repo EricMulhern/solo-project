e52fec103873e3e21f2c454daaa17a8c149c619b
Eric Mulhern <eric.mulhern@gmail.com>
2021-09-11 13:35:09 -0400

    initialized new repo with webpack framework copied from build tools unit

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, './client/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          {
+            loader: "sass-loader",
+            options: {
+              // Prefer `dart-sass`
+              implementation: require("sass"),
+            },
+          },
+        ],
+      },
+    ]
+  },
+  devServer: {
+    static: {
+      directory: __dirname
+      // directory: path.resolve(__dirname, 'build'),
+      // publicPath: '/build/', // not necessary?
+    },
+    // publicPath: '/build',
+    // contentBase: path.resolve(__dirname, 'build'),
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000/'
+      }
+    },
+    // compress: true,
+    // port: 8080,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: './index.html'
+    }),
+  ],
+};
